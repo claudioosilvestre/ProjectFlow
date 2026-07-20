2cb983d57a909f2a0d809263ef3dbce7955259dd
Cláudio Silvestre <claudioo.silvestre96@gmail.com>
2026-07-20 11:27:41 +0100

    feat(application): implement create project use case

--- src/ProjectFlow.Application/Projects/CreateProject/CreateProjectCommandHandler.cs
+++ src/ProjectFlow.Application/Projects/CreateProject/CreateProjectCommandHandler.cs
@@ -1,3 +1,4 @@
+using ProjectFlow.Application.Common.Interfaces;
 using ProjectFlow.Domain.Entities;
 
 namespace ProjectFlow.Application.Projects.CreateProject;
@@ -11,10 +12,19 @@ public class CreateProjectCommandHandler
         _projectRepository = projectRepository;
     }
 
-    public ProjectResponse Handle(CreateProjectCommand command)
+    public async Task<ProjectResponse> Handle(CreateProjectCommand command)
     {
         var project = new Project(command.Name, command.Description);
 
-        _projectRepository.Save(project);
+        
+        await _projectRepository.AddAsync(project);
+
+        return new ProjectResponse(
+            project.Id,
+            project.Name,
+            project.Description,
+            project.Status,
+            project.CreatedAt
+        );
     }
 }
